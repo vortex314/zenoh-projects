88030618d48faf11fbabc75e8c9263e65fce6890
Lieven Merckx <lieven.merckx@ing.com>
2025-02-12 17:35:30 +0100

    sync

--- zenoh-ota/src/main.rs
+++ zenoh-ota/src/main.rs
@@ -4,22 +4,46 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-use anyhow::{Result, Context};
+use anyhow::{Context, Error, Result};
 use clap::{Arg, Command};
-use log::info;
+use log::{error, info};
+use zenoh_config::WhatAmI;
 use std::fs;
 use std::path::Path;
+use walkdir::WalkDir;
 use zenoh::{bytes::Encoding, key_expr::KeyExpr, session, Config};
 
 mod logger;
 use logger::init;
 
 #[tokio::main]
-async fn main()  -> Result<()> {
+async fn main() -> Result<()> {
     zenoh::init_log_from_env_or("info");
     logger::init();
 
-    let config = Config::from_file("./config.json5")?;
+    let r = do_ota().await;
+    if r.is_err() {
+        error!("Error: {:?}", r.err().unwrap().to_string());
+    }
+    Ok(())
+}
+
+fn find_default_file() -> Result<String> {
+    let default_file = ".pio/build/esp32dev/firmware.bin";
+    // walk the directory tree from . to look for regexp match
+    for entry in WalkDir::new(".") {
+        let entry = entry.unwrap();
+        if entry.file_name().to_string_lossy().contains("firmware.bin") {
+            return Ok(entry.path().to_str().unwrap().to_string());
+        }
+    }
+    Ok(default_file.to_string())
+}
+
+async fn do_ota() -> Result<()> {
+    /*let config = Config::from_file("./config.json5")
+    .map_err(|e| anyhow::anyhow!(e))
+    .context("Error on Zenoh config ") ?; // Load the Zenoh configuration from a file*/
     // Parse command-line arguments
     let matches = Command::new("OTA Zenoh Uploader")
         .version("1.0")
@@ -29,7 +53,7 @@ async fn main()  -> Result<()> {
                 .short('k')
                 .long("key")
                 .value_name("KEY")
-                .default_value(".pio/build/esp32dev/firmware.bin")
+                .default_value("dst/cam1/ota/firmware")
                 .help("The Zenoh key (topic) to publish the firmware binary to")
                 .required(true),
         )
@@ -40,14 +64,35 @@ async fn main()  -> Result<()> {
                 .value_name("BINARY")
                 .default_value(".pio/build/esp32dev/firmware.bin")
                 .help("The path to the firmware binary file")
-                .required(true),
+                .required(false),
+        )
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .value_name("config")
+                .help("The path to the zenoh config file")
+                .required(false),
         )
         .get_matches();
 
     // Get the Zenoh key and binary path from the arguments
-    let key = matches.get_one::<String>("key").ok_or(Err(anyhow::anyhow!("")))?;
-    let binary_path = matches.get_one::<String>("binary").ok_or(Err(anyhow::anyhow!("")))?;
-    let binary_path = Path::new(binary_path);
+    let key = matches.get_one::<String>("key").context("argument key")?;
+    let binary_path = match matches.get_one::<String>("binary") {
+        Some(binary_path) => binary_path.clone(),
+        None => find_default_file()?,
+    };
+    let binary_path = Path::new(&binary_path);
+
+    let config = match matches.get_one::<String>("config") {
+        Some(c) => Config::from_file(c).map_err(|e| anyhow::anyhow!(e))?,
+        None => {
+            let mut config = Config::default();
+            config.insert_json5("mode", r#""client""#).unwrap();
+    //        config.insert_json5("mode", "\"client\"").unwrap();
+            config
+        }
+    };
 
     // Check if the firmware binary exists
     if !binary_path.exists() {
@@ -66,16 +111,19 @@ async fn main()  -> Result<()> {
     );
 
     info!("Opening session...");
-    let session = zenoh::open(config).await.map(|| Err( anyhow::anyhow!("")))?; // Open a Zenoh session
+    let session = zenoh::open(config).await.map_err(|e| anyhow::anyhow!(e))?; // Open a Zenoh session
 
     // Publish the firmware binary to the specified Zenoh key
     info!("Publishing firmware binary to key: {}", key);
-    session.put(key, firmware_data).await?;
+    session
+        .put(key, firmware_data)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
 
     info!("Firmware binary published successfully!");
 
     // Close the Zenoh session
-    session.close().await?;
+    session.close().await.map_err(|e| anyhow::anyhow!(e))?;
 
     Ok(())
 }
