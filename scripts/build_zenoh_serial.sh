0d516847231e0041689e46b2858a21e240202cc7
Lieven Merckx <lieven.merckx@ing.com>
2025-03-17 19:20:04 +0100

    extended scripts

--- scripts/build_zenoh_serial.sh
+++ scripts/build_zenoh_serial.sh
@@ -1,5 +1,46 @@
-cd ../../zenoh
-git checkout 1.2.1
-cargo build --release --all-targets --no-default-features --features transport_tcp,transport_serial
-cargo build -j 3 --release --all-targets --no-default-features --features transport_tcp,transport_serial
 
+
+#!/bin/bash
+cd ../../zenoh
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Build zenoh menu"
+TITLE="Zenoh build scope"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "zenohd z_sub withs serial and tcp "
+         2 "all-targets with serial and tcp "
+         3 "Like 2 only 3 cores")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            cargo build --release  --bin zenohd --example z_sub --features transport_tcp,transport_serial
+            ;;
+        2)
+            cargo build --release  --all-targets --no-default-features --features transport_tcp,transport_serial
+            ;;
+        3)
+            cargo build -j 3 --release --all-targets --no-default-features --features transport_tcp,transport_serial
+            ;;
+esac
+done
+exit
+
+git checkout 1.2.1
+cargo build --release  --all-targets --no-default-features --features transport_tcp,transport_serial
+
+cargo build --release  --bin zenohd --example z_sub --features transport_tcp,transport_serial
+
+248
+
+#!/bin/bash
