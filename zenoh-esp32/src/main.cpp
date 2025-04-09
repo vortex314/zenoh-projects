a32da577f93135147acac46dc2ae6ce355f95e7c
lieven <lieven.merckx@gmail.com>
2025-04-09 23:42:04 +0200

    sync

--- zenoh-esp32/src/main.cpp
+++ zenoh-esp32/src/main.cpp
@@ -85,7 +85,7 @@ extern "C" void app_main()
   esp_wifi_set_ps(WIFI_PS_NONE);
   // esp_coex_preference_set(ESP_COEX_PREFER_BALANCE);
 
-  zenoh_actor.prefix("esp1"); // set the zenoh prefix to src/esp1 and destination subscriber dst/esp1/**
+  zenoh_actor.prefix("esp3"); // set the zenoh prefix to src/esp3 and destination subscriber dst/esp3/**
 
   // WIRING the actors together
   // WiFi connectivity starts and stops zenoh connection
@@ -100,34 +100,34 @@ extern "C" void app_main()
 
   // publish data from actors
   wifi_actor.on_event([&](WifiEvent event)
-                      { zenoh_publish("src/esp1/wifi", event.serdes);
-                      zenoh_publish("info/esp1/wifi",event.prop_info); });
+                      { zenoh_publish("src/esp3/wifi", event.serdes);
+                      zenoh_publish("info/esp3/wifi",event.prop_info); });
   sys_actor.on_event([&](SysEvent event)
-                     { zenoh_publish("src/esp1/sys", event.serdes); 
-                     zenoh_publish("info/esp1/sys",event.prop_info); });
+                     { zenoh_publish("src/esp3/sys", event.serdes); 
+                     zenoh_publish("info/esp3/sys",event.prop_info); });
   zenoh_actor.on_event([&](ZenohEvent event) // send to myself
-                       { zenoh_publish("src/esp1/zenoh", event.serdes); 
-                       zenoh_publish("info/esp1/zenoh",event.prop_info); });
+                       { zenoh_publish("src/esp3/zenoh", event.serdes); 
+                       zenoh_publish("info/esp3/zenoh",event.prop_info); });
   ota_actor.on_event([&](OtaEvent event)
-                     { zenoh_publish("src/esp1/ota", event.serdes); });
+                     { zenoh_publish("src/esp3/ota", event.serdes); });
 
   // send commands to actors coming from zenoh, deserialize and send to the right actor
   zenoh_actor.on_event([&](ZenohEvent event)
                        {
     if (event.publish) {
-      if (event.publish->topic == "dst/esp1/sys") {
+      if (event.publish->topic == "dst/esp3/sys") {
         auto msg = cbor_deserialize<SysMsg>(event.publish->payload);
         if ( msg )  sys_actor.tell(new SysCmd{.serdes = PublishSerdes ( msg.value() )});  
       } 
-      else if ( event.publish->topic == "dst/esp1/wifi") {
+      else if ( event.publish->topic == "dst/esp3/wifi") {
         auto msg = cbor_deserialize<WifiMsg>(event.publish->payload);
         if ( msg ) wifi_actor.tell(new WifiCmd{.serdes = PublishSerdes ( msg.value() )});
       } 
-      else if ( event.publish->topic == "dst/esp1/zenoh") {
+      else if ( event.publish->topic == "dst/esp3/zenoh") {
         auto msg = cbor_deserialize<ZenohMsg>(event.publish->payload);
         if ( msg ) zenoh_actor.tell(new ZenohCmd{.serdes = PublishSerdes ( msg.value() )});
       } 
-      else if ( event.publish->topic == "dst/esp1/ota") {
+      else if ( event.publish->topic == "dst/esp3/ota") {
         cbor_deserialize<OtaMsg>(event.publish->payload) >> [&](OtaMsg msg) {ota_actor.tell(new OtaCmd{.msg = std::move(msg)});};
       } 
       else {
