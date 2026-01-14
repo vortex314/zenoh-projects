3d9e775d82ed83ba54f7819f833ba67a6edf2765
Lieven Merckx <lieven.merckx@gmail.com>
2026-01-14 23:46:52 +0100

    sync
    Signed-off-by: Lieven Merckx <lieven.merckx@gmail.com>

---
+++ limeros/install.sh
@@ -0,0 +1,33 @@
+sudo apt install libgtk-3-dev libwebkit2gtk-4.0-dev libayatana-appindicator3-dev build-essential
+ 1947  sudo apt update
+ 1948  sudo apt install libgtk-3-dev libwebkit2gtk-4.0-dev libayatana-appindicator3-dev build-essential
+ 1949  sudo apt install libgtk-3-dev libwebkit2gtk-dev libayatana-appindicator3-dev build-essential
+ 1950  apt search webkitgtk
+ 1951  sudo apt install libwebkit2gtk-4.1-dev
+ 1952  sudo apt install libwebkit2gtk-4.1-dev libgtk-3-dev
+ 1953  apt search libjavascript
+ 1954  sudo apt install libjavascriptcoregtk-4.1-dev
+ 1955  sudo apt install libjavascriptcoregtk-4.1-dev --fix-missing
+ 1956  sudo apt install libwebkit2gtk-4.1-dev libgtk-3-dev --fix-missing
+ 1958  sudo apt-get update --error-on-any
+ 1959  sudo apt-get install -y libwebkit2gtk-4.1-dev libgtk-3-dev
+ 1961  sudo apt install libjavascriptcoregtk-4.1-dev --fix-missing
+ 1962  sudo apt autoremove
+ 1963  sudo apt-get clean
+ 1964  sudo apt-get update --error-on-any
+ 1965  sudo apt-get install -y libwebkit2gtk-4.1-dev libgtk-3-dev
+ 1966  sudo apt-get install -y libwebkit2gtk-4.1-dev libgtk-3-dev --fix-missing
+ 1969  sudo apt-get upgrade -y
+ 1970  sudo apt-get install -y pkg-config libwebkit2gtk-4.1-dev libgtk-3-dev
+ 1972  sudo apt install libsoup-dev
+ 1973  apt search libsoup
+ 1974  sudo apt install libsoup-2.4.1-dev
+ 1975  sudo apt install libsoup-2.4.1
+ 1976  sudo apt install libsoup-2.4-1
+ 1977  sudo apt install libsoup2.4-dev
+ 1978  sudo apt install libwebkit2gtk-4.1
+ 1979  sudo apt install libwebkit2gtk-4.1-dev
+ 1980  apt search webkit2gtk-4.1
+ 1981  sudo apt install libwebkit2gtk-4.1-0
+ 1983  sudo apt-getupdate
+ sudo apt-get install -y pkg-config libwebkit2gtk-4.1-dev libgtk-3-dev
